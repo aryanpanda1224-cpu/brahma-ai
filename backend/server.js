463320b51909b7972c7b2a2ed4bd152b28bef59d
aryanpanda1224-cpu <aryanpanda1224@gmail.com>
2026-09-17 17:00:08 +0530

    Upgrade BRAHMA backend API

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain"
+  // Allow BRAHMA website to communicate with the backend
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+      status: "online",
+      message: "BRAHMA backend is working!"
+    }));
+
+    return;
+  }
+
+  res.writeHead(404, {
+    "Content-Type": "application/json"
   });
 
-  res.end("BRAHMA backend is online!");
+  res.end(JSON.stringify({
+    error: "Not found"
+  }));
 });
 
 server.listen(3000, () => {
