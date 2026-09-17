ca6ed438eae5d81decc565f05c005b75dfe49abb
aryanpanda1224-cpu <aryanpanda1224@gmail.com>
2026-09-17 16:53:52 +0530

    Create basic BRAHMA backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("BRAHMA backend is online!");
+});
+
+server.listen(3000, () => {
+  console.log("BRAHMA backend running on port 3000");
+});
